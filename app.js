bf8e37f60ebc3b035fd8256bceea163007b361e4
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-01-18 20:51:58 +0200

    another test

--- app.js
+++ app.js
@@ -5,11 +5,8 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
-const INDEX = '/index.html';
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = express();
 
 const io = socketIO(server);
 
