fbfbf433e8828386675385674b19e4e0aaf8e0d3
Daryoush Farsimadan <farsimadan.daryoush@gmail.com>
2020-02-03 16:14:25 +0200

    removed dead code

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("Express");
-const router = express.Router();
-
-router.get("/", (req, res ) => {
-    res.send({response: "Connection ok."}).status(200);
-});
-
-module.exports = router;
